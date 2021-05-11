2badfc11a6bf92527cc635c9d42128fa283c20b2
sancppp <2096067752@qq.com>
2021-05-11 16:34:01 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,104 +1,57 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+//-----pre_def----
+const double PI = acos(-1.0);
+const int INF = 0x3f3f3f3f;
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+#define fir(i, a, b) for (int i = (a); i <= (b); i++)
+#define rif(i, a, b) for (int i = (a); i >= (b); i--)
+#define endl '\n'
+#define init_h memset(h, -1, sizeof h), idx = 0;
+#define lowbit(x) x &(-x)
 
-const int N = 1e5 + 10;
-
-double a[N], b[N];
-
-int n, m, k, t;
-struct cmp
+//---------------
+const int N = 1e2 + 10;
+int n, tr[N][N][N];
+void update(int x, int y, int z, int d)
 {
-    bool operator()(double a, double b) const
-    {
-        return a > b;
-    }
-};
-
-double f(int x)
-{ // x 个上午取室内
-    //cout << x << ":" << endl;
-    double ans = 0;
-    multiset<double,cmp> q1, q2;
-    for (int i = 1; i <= n; i++)
-    {
-        q1.insert(a[i]);
-    }
-    for (int i = 1; i <= m; i++)
-    {
-        q2.insert(b[i]);
-    }
-    for (int i = 1; i <= x; i++)
-    {
-        double cur = *q1.begin();
-        
-        ans += cur;
-        q1.erase(q1.begin());
-        q1.insert(cur * 0.6);
-    }
-    for (int i = 1; i <= t - k - x; i++)
-    {
-        double cur = *q2.begin();
-        
-        ans += cur;
-        q2.erase(q2.begin());
-        q2.insert(cur * 0.6);
-    }
-    for (int i = 1; i <= k; i++)
-    {
-        if (*q1.begin() > *q2.begin() * 0.8)
-        {
-            double cur = *q1.begin();
-            
-            ans += cur;
-            q1.erase(q1.begin());
-            q1.insert(cur * 0.6);
-        }
-        else
-        {
-            double cur = *q2.begin();
-            
-            ans += cur * 0.8;
-            q2.erase(q2.begin());
-            q2.insert(cur * 0.6);
-        }
-    }
-    return ans;
+    for (int i = x; i <= n; i += lowbit(i))
+        for (int j = y; j <= n; j += lowbit(j))
+            for (int k = z; k <= n; k += lowbit(k))
+                tr[i][j][k] += d;
 }
-
+LL query(int x, int y, int z)
+{
+    LL ret = 0;
+    for (int i = x; i > 0; i -= lowbit(i))
+        for (int j = y; j > 0; j -= lowbit(j))
+            for (int k = z; k > 0; k -= lowbit(k))
+                ret += tr[i][j][k];
+    return ret;
+}
+//无区间修改函数。。
+LL solve(int x, int y, int z, int xx, int yy, int zz)
+{
+    return query(xx, yy, zz) - query(xx, yy, z - 1) - query(xx, y - 1, zz) - query(x - 1, yy, zz) + query(xx, y - 1, z - 1) + query(x - 1, yy, z - 1) + query(x - 1, y - 1, zz) - query(x - 1, y - 1, z - 1);
+}
 int main()
 {
+#ifndef ONLINE_JUDGE
     freopen("in.txt", "r", stdin);
     freopen("out.txt", "w", stdout);
-    scanf("%d %d %d %d", &n, &m, &t, &k);
-    for (int i = 1; i <= n; i++)
+    int StartTime = clock();
+#endif
+    int T = 1;
+    scanf("%d", &T);
+    while (T--)
     {
-        scanf("%lf", &a[i]);
     }
-    for (int i = 1; i <= m; i++)
-    {
-        scanf("%lf", &b[i]);
-    }
-    int l = 0, r = t - k;
-    while (l + 5 <= r)
-    {
-        int lmid = l + (r - l) / 3, rmid = r - (r - l) / 3;
-        if (f(lmid) > f(rmid))
-        {
-            r = rmid;
-        }
-        else
-        {
-            l = lmid;
-        }
-    }
-    double ans = 0;
-    // cout << l << " " << r << "\n";
-    for (int i = l; i <= r; i++)
-    {
-        ans = max(ans, f(i));
-        // cout << f(i) << "\n";
-    }
-    printf("%.2f\n", ans);
+#ifndef ONLINE_JUDGE
+    printf("Run_Time = %d ms\n", clock() - StartTime);
+#endif
     return 0;
 }
