3bfbf26cf1b6170c41713ee022913c4ab90ded66
sancppp <2096067752@qq.com>
2021-05-24 20:37:27 +0800

    ????

--- test.cpp
+++ test.cpp
@@ -1,61 +1,31 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
+// #define int long long
+#define PI pair<int,int>
 using namespace std;
-//-----pre_def----
-const double PI = acos(-1.0);
-const int INF = 0x3f3f3f3f;
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef pair<int, int> PII;
-typedef pair<double, double> PDD;
-#define fir(i, a, b) for (int i = (a); i <= (b); i++)
-#define rif(i, a, b) for (int i = (a); i >= (b); i--)
-#define endl '\n'
-#define init_h memset(h, -1, sizeof h), idx = 0;
-#define lowbit(x) x &(-x)
+const int N = 1e7 + 10;
 
-//---------------
-const int N = 1e5+10;
-struct node
-{
-    int l,r;
-    bool operator < (const node & b)const{
-        if(l!=b.l)  return l<b.l;
-        return r<b.r;
-    }
-};
+int prime[N], cnt;
 
-multiset<node> s; 
-void init(){}
-void print()
-{
-    for(auto t: s)
-    {
-        cout<<t.l<<" "<<t.r<<endl;
+bool st[N];
+
+void init() {
+    for (int i = 2; i < N; i++) {
+        if (!st[i]) {
+            prime[++cnt] = i;
+        }
+        for (int j = 1; j <= cnt && 1ll * i * prime[j] < N; j++) {
+            st[i * prime[j]] = 1;
+            if (i % prime[j] == 0) {
+                break;
+            }
+        }
     }
-    puts("");
 }
-int main()
-{
-#ifndef ONLINE_JUDGE
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-    int StartTime = clock();
-#endif
-    s.insert({1,1});
-    s.insert({1,1});
-    s.insert({2,1});
-    s.insert({1,-1});
-    print();
-    //只删除一个
-    s.erase(s.find({1,1}));
-    print();
-    //删除全部相同的元素
-    s.insert({1,1});
-    s.erase({1,1});
-    print();
-#ifndef ONLINE_JUDGE
-    printf("Run_Time = %d ms\n", clock() - StartTime);
-#endif
+
+signed main(){
+    init();
+    for (int i = 1; i <= cnt; i++) {
+        printf("%d\n", prime[i]);
+    }
     return 0;
 }
